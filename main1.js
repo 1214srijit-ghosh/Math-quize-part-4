dd44f9984f44316fa5fc491922e17b9a475f86f3
1214srijit-ghosh <103191272+1214srijit-ghosh@users.noreply.github.com>
2022-07-17 16:45:40 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,10 @@
+function addUser()
+{
+    var player1_name = document.getElementById("user1").value;
+    var player2_name = document.getElementById("user2").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location = "index2.html";
+}
